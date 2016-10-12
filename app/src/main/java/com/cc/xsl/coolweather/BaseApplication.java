052cd85aa40d75a6d58c29e707651bc90a3a7082
shuailong <xushuailong@111.com.cn>
2016-10-12 17:33:44 +0800

    update

--- app/src/main/java/com/cc/xsl/coolweather/BaseApplication.java
+++ app/src/main/java/com/cc/xsl/coolweather/BaseApplication.java
@@ -2,6 +2,7 @@ package com.cc.xsl.coolweather;
 
 import android.app.Activity;
 import android.app.Application;
+import android.content.Context;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,19 +14,26 @@ import java.util.List;
 public class BaseApplication extends Application {
     public boolean isDebug = true;
     private static BaseApplication app;
+    private static Context context;
     private List<Activity> activities = new ArrayList<>();
 
     @Override
     public void onCreate() {
         super.onCreate();
         app = this;
+        context = getApplicationContext();
     }
 
+    public static Context getContext(){
+        return context;
+    }
     public static BaseApplication getApp(){
         return app;
     }
     public void addActivity(Activity activity){
-        activities.add(activity);
+        if (!activities.contains(activity)) {
+            activities.add(activity);
+        }
     }
     public void removeActivity(Activity activity){
         activities.remove(activity);
