b74d7665ee2a24fd5e9087e08d211bece2dfc603
shuailong <xushuailong@111.com.cn>
2018-06-04 18:33:44 +0800

    cache与files目录

--- app/src/main/java/com/cc/xsl/coolweather/fragment/OtherFragment.java
+++ app/src/main/java/com/cc/xsl/coolweather/fragment/OtherFragment.java
@@ -8,15 +8,28 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.cc.xsl.coolweather.R;
 import com.cc.xsl.coolweather.util.LogUtil;
+import com.cc.xsl.coolweather.util.ToastUtil;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 
 /**
  * Created by xushuailong on 2016/10/11.
  */
 
-public class OtherFragment extends Fragment {
+public class OtherFragment extends Fragment implements View.OnClickListener {
+
+    private Button saveFilesBtn, saveCachesBtn, getFilesBtn, getCachesBtn;
 
     @Override
     public void onAttach(Context context) {
@@ -41,7 +54,23 @@ public class OtherFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_other, container, false);
         LogUtil.d("OtherFragment onCreateView");
+        initViews(view);
+        viewEvents();
         return view;
+    }
+
+    private void viewEvents() {
+        saveCachesBtn.setOnClickListener(this);
+        saveFilesBtn.setOnClickListener(this);
+        getFilesBtn.setOnClickListener(this);
+        getCachesBtn.setOnClickListener(this);
+    }
+
+    private void initViews(View view) {
+        saveCachesBtn = (Button) view.findViewById(R.id.save_caches_btn);
+        saveFilesBtn = (Button) view.findViewById(R.id.save_files_btn);
+        getCachesBtn = (Button) view.findViewById(R.id.get_caches_btn);
+        getFilesBtn = (Button) view.findViewById(R.id.get_files_btn);
     }
 
     @Override
@@ -91,5 +120,89 @@ public class OtherFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         LogUtil.d("OtherFragment onDetach");
+    }
+
+    /**
+     * Environment.getDataDirectory() = /data
+     * Environment.getDownloadCacheDirectory() = /cache
+     * Environment.getExternalStorageDirectory() = /mnt/sdcard
+     * Environment.getExternalStoragePublicDirectory(“test”) = /mnt/sdcard/test
+     * Environment.getRootDirectory() = /system
+     * getPackageCodePath() = /data/app/com.my.app-1.apk
+     * getPackageResourcePath() = /data/app/com.my.app-1.apk
+     * getCacheDir() = /data/data/com.my.app/cache
+     * getDatabasePath(“test”) = /data/data/com.my.app/databases/test
+     * getDir(“test”, Context.MODE_PRIVATE) = /data/data/com.my.app/app_test
+     * getExternalCacheDir() = /mnt/sdcard/Android/data/com.my.app/cache
+     * getExternalFilesDir(“test”) = /mnt/sdcard/Android/data/com.my.app/files/test
+     * getExternalFilesDir(null) = /mnt/sdcard/Android/data/com.my.app/files
+     * getFilesDir() = /data/data/com.my.app/files
+     */
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.save_files_btn: {
+                try {
+//                    OutputStream saveFileOut = getActivity().openFileOutput("file_data", Context.MODE_WORLD_WRITEABLE);
+                    ToastUtil.showMessage(getActivity().getDir("test", Context.MODE_WORLD_WRITEABLE).getAbsolutePath());
+                    OutputStream saveFileOut = new FileOutputStream(new File(getActivity().getFilesDir(), "file_data"));
+                    saveFileOut.write("this is files \n这里是文件夹".getBytes("utf-8"));
+                    saveFileOut.flush();
+                    saveFileOut.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+            case R.id.save_caches_btn: {
+                OutputStream saveFileOut = null;
+                try {
+                    saveFileOut = new FileOutputStream(new File(getActivity().getCacheDir(), "cache_file"));
+                    saveFileOut.write("this is caches \n这里是缓存文件".getBytes("utf-8"));
+                    saveFileOut.flush();
+                    saveFileOut.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+            case R.id.get_files_btn: {
+                try {
+                    InputStream getFileIn = getActivity().openFileInput("file_data");
+                    byte[] buffer = new byte[getFileIn.available()];
+                    getFileIn.read(buffer);
+                    getFileIn.close();
+                    ToastUtil.showMessage(new String(buffer));
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+            case R.id.get_caches_btn: {
+                try {
+                    InputStream getCacheIn = new FileInputStream(new File(getActivity().getCacheDir(), "cache_file"));
+                    byte[] buffer = new byte[getCacheIn.available()];
+                    getCacheIn.read(buffer);
+                    getCacheIn.close();
+                    ToastUtil.showMessage(new String(buffer));
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+        }
     }
 }
