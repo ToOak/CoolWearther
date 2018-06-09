0c34a227918fa4b7768d9a193bf0a737b56ab065
afei <xuafeiJelly@163.com>
2018-06-09 18:56:31 +0800

    update

--- app/src/main/java/com/cc/xsl/coolweather/fragment/OtherFragment.java
+++ app/src/main/java/com/cc/xsl/coolweather/fragment/OtherFragment.java
@@ -1,12 +1,12 @@
 package com.cc.xsl.coolweather.fragment;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
