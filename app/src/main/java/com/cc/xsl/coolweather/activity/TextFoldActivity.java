b61ec296e1f1eeea03857b32b96f3e98f4756679
shuailong <xushuailong@111.com.cn>
2018-06-21 17:34:27 +0800

    TextView多行省略

--- app/src/main/java/com/cc/xsl/coolweather/activity/TextFoldActivity.java
+++ app/src/main/java/com/cc/xsl/coolweather/activity/TextFoldActivity.java
@@ -2,6 +2,10 @@ package com.cc.xsl.coolweather.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Paint;
+import android.os.Build;
+import android.text.TextPaint;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.Button;
@@ -16,12 +20,13 @@ import com.cc.xsl.coolweather.util.LogUtil;
 public class TextFoldActivity extends BaseActivity implements View.OnClickListener {
     public static final String CONTENT = "content";
     private Button moreBtn;
-    private TextView descShortTv, descLongTv;
+    private TextView descShortTv, descLongTv, originalTv, resultTv;
     private FrameLayout descFl;
     private String content = "";
     private ImageView moreLineIv;
     private boolean isInited = false;
     private boolean isShowShortText = true;
+    private String s = "";
 
     public static Intent getIntent(Context context, String content) {
         Intent intent = new Intent(context, TextFoldActivity.class);
@@ -31,8 +36,24 @@ public class TextFoldActivity extends BaseActivity implements View.OnClickListen
 
     @Override
     protected void loadData() {
+        s = "012345678901234567890123456789012345678901234567890123456789012345678901234567890" +
+                "12345678901234567890123456789012345678901234567890123456789";
         descShortTv.setText(content);
         descLongTv.setText(content);
+        originalTv.setText(s);
+        originalTv.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                LogUtil.e("onGlobalLayout");
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    originalTv.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                } else {
+                    originalTv.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                }
+
+//                initResultTv();
+            }
+        });
 
         ViewTreeObserver viewTreeObserver = descFl.getViewTreeObserver();
         viewTreeObserver.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
@@ -53,6 +74,21 @@ public class TextFoldActivity extends BaseActivity implements View.OnClickListen
                 return true;
             }
         });
+    }
+
+    private void initResultTv() {
+        TextPaint paint = originalTv.getPaint();
+        int paddingLeft = originalTv.getPaddingLeft();
+        int paddingRight = originalTv.getPaddingRight();
+        //给省略号留的长度（但是，因为字符占位问题，获取的这个长度，要比省略号的三个点的长度大一些）
+        float moreTxt = originalTv.getTextSize() * 3;
+        //乘2，是代表2行的意思，减去moreText，是给省略号预留一点位置
+        float availableTextWidth = (originalTv.getWidth() - paddingLeft - paddingRight) * 2 ;
+        CharSequence ellipsizeStr = TextUtils.ellipsize(s, paint, availableTextWidth, TextUtils.TruncateAt.END);
+        resultTv.setText(ellipsizeStr);
+
+        LogUtil.e("paddingLeft: " + paddingLeft + "\tpaddingRight: "
+                + paddingRight + "\tmoreTxt:" + moreTxt + "\tellipsizeStr: " + ellipsizeStr);
     }
 
     /**
@@ -80,6 +116,8 @@ public class TextFoldActivity extends BaseActivity implements View.OnClickListen
         descShortTv = (TextView) findViewById(R.id.desc_short_tv);
         descFl = (FrameLayout) findViewById(R.id.desc_fl);
         moreLineIv = (ImageView) findViewById(R.id.more_line_iv);
+        originalTv = (TextView) findViewById(R.id.original_tv);
+        resultTv = (TextView) findViewById(R.id.result_tv);
 
         moreBtn.setOnClickListener(this);
     }
@@ -127,5 +165,13 @@ public class TextFoldActivity extends BaseActivity implements View.OnClickListen
         } else {
             moreBtn.setText(moreText);
         }
+    }
+
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        LogUtil.e("onWindowFocusChanged");
+        initResultTv();
     }
 }
