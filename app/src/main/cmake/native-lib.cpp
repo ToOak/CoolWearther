6c40251deffa52d2b6dcb2dd1e9987f4a88d2d61
xushuailong <xuafeiJelly123@outlook.com>
2018-12-27 17:05:07 +0800

    jni添加log

--- app/src/main/cmake/native-lib.cpp
+++ app/src/main/cmake/native-lib.cpp
@@ -1,5 +1,10 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
+
+#define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, "oak", __VA_ARGS__))
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "oak", __VA_ARGS__))
+#define LOGV(...)  ((void)__android_log_print(ANDROID_LOG_VERBOSE, "oak", __VA_ARGS__))
 
 /**
  * 通过：
@@ -16,5 +21,6 @@ Java_com_cc_xsl_coolweather_CMakeTest_stringFromCpp(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello CMake from C++";
+    LOGE("cmake string from cpp");
     return env->NewStringUTF(hello.c_str());
 }
